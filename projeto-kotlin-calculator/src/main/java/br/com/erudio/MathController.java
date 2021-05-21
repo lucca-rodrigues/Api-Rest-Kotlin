0180779eed2cf4af1a2caee98e084086757fd882
Lucca Rodrigues <contato.luccarodrigues@gmail.com>
2021-05-21 14:05:47 -0300

    add exceptions

--- projeto-kotlin-calculator/src/main/java/br/com/erudio/MathController.java
+++ projeto-kotlin-calculator/src/main/java/br/com/erudio/MathController.java
@@ -1,32 +1,32 @@
 package br.com.erudio;
-
+import br.com.erudio.exception.UnsuportedMathOperationException;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class MathController {
- 
- @RequestMapping(value="/sum/{numberOne}/{numberTwo}", method= RequestMethod.GET)
- public Double sum(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) throws Exception {
-  if(!isNumeric(numberOne) || !isNumeric(numberTwo)){
-   throw new Exception();
-  }
-  Double sum = convertToDouble(numberOne) + convertToDouble(numberTwo);
-	 return sum;
- }
+  @RestController
+  public class MathController {
 
- private Double convertToDouble(String stringNumber){
-  if(stringNumber == null) return  0D;
-  String number = stringNumber.replace(",",".");
-  if(isNumeric(number)) return Double.parseDouble(number);
-  return 0D;
- }
+   @RequestMapping(value="/sum/{numberOne}/{numberTwo}", method= RequestMethod.GET)
+   public Double sum(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) throws Exception {
+    if(!isNumeric(numberOne) || !isNumeric(numberTwo)){
+     throw new UnsuportedMathOperationException("Please set a numeric value");
+    }
+    Double sum = convertToDouble(numberOne) + convertToDouble(numberTwo);
+    return sum;
+   }
 
- private boolean isNumeric(String stringNumber){
-  if(stringNumber == null) return false;
-  String number = stringNumber.replaceAll(",",".");
-  return number.matches("[-+]?[0-9]*\\.?[0-9]+");
- }
+   private Double convertToDouble(String stringNumber){
+    if(stringNumber == null) return  0D;
+    String number = stringNumber.replace(",",".");
+    if(isNumeric(number)) return Double.parseDouble(number);
+    return 0D;
+   }
+
+   private boolean isNumeric(String stringNumber){
+    if(stringNumber == null) return false;
+    String number = stringNumber.replaceAll(",",".");
+    return number.matches("[-+]?[0-9]*\\.?[0-9]+");
+   }
 }
