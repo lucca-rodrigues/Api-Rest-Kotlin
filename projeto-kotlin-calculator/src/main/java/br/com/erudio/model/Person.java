f6c64fbe82332200b6ccd315b06c7b513651a150
Lucca Rodrigues <contato.luccarodrigues@gmail.com>
2021-05-24 23:06:20 -0300

    add persons

--- projeto-kotlin-calculator/src/main/java/br/com/erudio/model/Person.java
+++ projeto-kotlin-calculator/src/main/java/br/com/erudio/model/Person.java
@@ -55,18 +55,6 @@ public class Person implements Serializable {
     }
 
     @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((address == null) ? 0 : address.hashCode());
-        result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
-        result = prime * result + ((gender == null) ? 0 : gender.hashCode());
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
-        result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
-        return result;
-    }
-
-    @Override
     public boolean equals(Object obj) {
         if (this == obj)
             return true;
