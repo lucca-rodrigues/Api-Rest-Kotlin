cce8775b1875ee658dc74507ff1f568878fa5b1d
Lucca Rodrigues <contato.luccarodrigues@gmail.com>
2021-05-24 23:00:10 -0300

    add persons

--- projeto-kotlin-calculator/src/main/java/br/com/erudio/Startup.java
+++ projeto-kotlin-calculator/src/main/java/br/com/erudio/Startup.java
@@ -3,10 +3,10 @@ package br.com.erudio;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class Startup {
- public static void main(String[] args) {
-	 SpringApplication.run(Startup.class, args);
- }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Startup.class, args);
+    }
 }
---
+++ projeto-kotlin-calculator/src/main/java/br/com/erudio/services/PersonServices.java
@@ -0,0 +1,23 @@
+package br.com.erudio.services;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.stereotype.Service;
+
+import br.com.erudio.model.Person;
+
+@Service
+public class PersonServices {
+
+    private final AtomicLong counter = new AtomicLong();
+
+    public Person findById(String id) {
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("Leandro");
+        person.setLastName("Costa");
+        person.setAddress("Uberlândia - Minas Gerais -Brasil");
+        person.setGender("Male");
+        return person;
+    }
+}
