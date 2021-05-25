7061dfe3e965868d962a7fbe7ba2e2b43ac612d2
Lucca Rodrigues <contato.luccarodrigues@gmail.com>
2021-05-25 20:16:22 -0300

    methods GET,POST,PUT

--- projeto-kotlin-calculator/src/main/java/br/com/erudio/controller/PersonController.java
+++ projeto-kotlin-calculator/src/main/java/br/com/erudio/controller/PersonController.java
@@ -3,10 +3,9 @@ import br.com.erudio.model.Person;
 import br.com.erudio.services.PersonServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
@@ -16,11 +15,31 @@ public class PersonController {
  @Autowired
  private PersonServices services;
 
+ @RequestMapping(method=RequestMethod.GET,
+         produces= MediaType.APPLICATION_JSON_VALUE)
+ public List<Person> findAll() {
+  return services.findAll();
+ }
+
  @RequestMapping(value="/{id}",
          method= RequestMethod.GET,
-         produces = MediaType.APPLICATION_JSON_VALUE)
+         produces= MediaType.APPLICATION_JSON_VALUE)
  public Person findById(@PathVariable("id") String id) {
   return services.findById(id);
+ }
+
+ @RequestMapping(method=RequestMethod.POST,
+         consumes= MediaType.APPLICATION_JSON_VALUE,
+         produces= MediaType.APPLICATION_JSON_VALUE)
+ public Person create(@RequestBody Person person) {
+  return services.create(person);
+ }
+
+ @RequestMapping(method=RequestMethod.PUT,
+         consumes= MediaType.APPLICATION_JSON_VALUE,
+         produces= MediaType.APPLICATION_JSON_VALUE)
+ public Person update(@RequestBody Person person) {
+  return services.create(person);
  }
 
 }
