1393b5bd945e64e8c86e539bdf1dd82228902bd7
Lucca Rodrigues <contato.luccarodrigues@gmail.com>
2021-05-31 22:02:34 -0300

    connection database and create Person Repository

--- projeto-kotlin-calculator/src/main/java/br/com/erudio/model/Person.java
+++ projeto-kotlin-calculator/src/main/java/br/com/erudio/model/Person.java
@@ -2,13 +2,34 @@ package br.com.erudio.model;
 
 import java.io.Serializable;
 
-public class Person implements Serializable {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name="person")
+public class Person implements Serializable{
+
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "first_name", nullable = false, length = 80)
     private String firstName;
+
+    @Column(name = "last_name", nullable = false, length = 80)
     private String lastName;
+
+    @Column(nullable = false, length = 100)
     private String address;
+
+    @Column(nullable = false, length = 6)
     private String gender;
 
     public Person() {
@@ -52,6 +73,18 @@ public class Person implements Serializable {
 
     public void setGender(String gender) {
         this.gender = gender;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((address == null) ? 0 : address.hashCode());
+        result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+        result = prime * result + ((gender == null) ? 0 : gender.hashCode());
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+        return result;
     }
 
     @Override
